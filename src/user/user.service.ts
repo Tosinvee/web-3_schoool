ee496b332b1ab547353410bdce055f70d8fd6a42
ceasar28 <emmanuelugochukwuekete@gmail.com>
2024-10-09 03:43:46 +0100

    user profile

---
+++ src/user/user.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
+import { Prisma } from '@prisma/client';
+
+@Injectable()
+export class UserService {
+  constructor(private readonly databaserService: DatabaseService) {}
+
+  async createUser(
+    createDto: Prisma.UserCreateInput,
+  ): Promise<Record<string, any>> {
+    try {
+      const createdUser = await this.databaserService.user.create({
+        data: createDto,
+      });
+      return { createdUser };
+    } catch (error) {
+      throw new error();
+    }
+  }
+
+  async getUser(id: number): Promise<Record<string, any>> {
+    try {
+      const userProfile = await this.databaserService.user.findFirst({
+        where: { id },
+      });
+
+      return { userProfile };
+    } catch (error) {
+      throw new error();
+    }
+  }
+
+  async updateUser(
+    id: number,
+    updateDto: Prisma.UserUpdateInput,
+  ): Promise<Record<string, any>> {
+    try {
+      const updatedUser = await this.databaserService.user.update({
+        where: { id },
+        data: updateDto,
+      });
+      return { updatedUser };
+    } catch (error) {
+      throw new error();
+    }
+  }
+}
